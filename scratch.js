5942bb876e1ff513dc89d74465fc88a9d88b136d
Youssef Rivas <Youssef.Rivas@gmail.com>
2021-07-07 10:45:08 -0500

    "sorted --> autofill/add functionality"

--- scratch.js
+++ scratch.js
@@ -1,3 +1,4 @@
+
 "use strict"
 
 // Main Roast Section
@@ -13,62 +14,74 @@ function renderCoffee(coffee) {
 
 function renderCoffees(coffees) {
     var html = '';
-    for (var i = 0; i < coffees.length; i++) {
+    for (var i = coffees.length - 1; i >= 0; i--) {
         html += renderCoffee(coffees[i]);
     }
     return html;
 }
 
-function updateCoffees(e) {
-    e.preventDefault(); // don't submit the form, we just want to update the data
+function updateCoffees() {
+    // e.preventDefault// don't submit the form, we just want to update the data
     var selectedRoast = roastSelection.value;
+    var selectedName = nameInput.value;
     var filteredCoffees = [];
+    JSON.parsee(window.localStorage.getItem('customCoffees'));
     coffees.forEach(function (coffee) {
-        if (coffee.roast === selectedRoast) {
-            filteredCoffees.push(coffee);
+        if (coffee.roast === selectedRoast || 'All' === selectedRoast) {
+            if(coffee.name.toLowerCase().includes(selectedName.toLowerCase())) {
+                filteredCoffees.push(coffee);
+            }   else if("" === selectedName) {
+                filteredCoffees.push(coffee)}
         }
     });
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
-];
+if(localStorage.getItem('customCoffees')=== null) {
+    var coffees = [
+        {id: 1, name: 'Light City', roast: 'light'},
+        {id: 2, name: 'Half City', roast: 'light'},
+        {id: 3, name: 'Cinnamon', roast: 'light'},
+        {id: 4, name: 'City', roast: 'medium'},
+        {id: 5, name: 'American', roast: 'medium'},
+        {id: 6, name: 'Breakfast', roast: 'medium'},
+        {id: 7, name: 'High', roast: 'dark'},
+        {id: 8, name: 'Continental', roast: 'dark'},
+        {id: 9, name: 'New Orleans', roast: 'dark'},
+        {id: 10, name: 'European', roast: 'dark'},
+        {id: 11, name: 'Espresso', roast: 'dark'},
+        {id: 12, name: 'Viennese', roast: 'dark'},
+        {id: 13, name: 'Italian', roast: 'dark'},
+        {id: 14, name: 'French', roast: 'dark'},
+    ];
+    coffees.reverse();
+} else {
+    var coffees = JSON.parse(window.localStorage.getItem('customCoffees'));
+    coffees.reverse();
+}
 
 
 var tbody = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
 var roastSelection = document.querySelector('#roast-selection');
-
+var nameInput = document.querySelector('#name-input')
+var roastSelection2 = document.querySelector('#roast-selection2');
 tbody.innerHTML = renderCoffees(coffees);
 
 submitButton.addEventListener('click', updateCoffees);
 
 //Ascending Order
 ////////////////////////////////////////////////////////////////////////
-coffees.sort(function compareCoffee(a, b) {
-    const value1 = a.name.toUpperCase();
-    const value2 = b.name.toUpperCase();
-    if (value1 < value2)
-        return -1
-    if (value1 > value2)
-        return 1
-    return 0
-});
+//coffees.sort(function compareCoffee(a, b) {
+//   const value1 = a.name.toUpperCase();
+//  const value2 = b.name.toUpperCase();
+// if (value1 < value2)
+//    return -1
+// if (value1 > value2)
+//   return 1
+// return 0
+//});
 
 
 ////////////////////////////////////////////////////////////////////////
