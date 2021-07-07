434f16a40dd6167241b0c64521797291dc727e99
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-07-07 12:10:32 -0500

    Updated Files

--- main.js
+++ main.js
@@ -73,6 +73,32 @@ coffees.sort(function compareCoffee(a, b) {
 
 ////////////////////////////////////////////////////////////////////////
 
+/*list of available options*/
+var n= coffees.length; //length of datalist tags
+
+function ac(value) {
+    document.getElementById('datalist').innerHTML = '';
+    //setting datalist empty at the start of function
+    //if we skip this step, same name will be repeated
+
+    let l;
+    l=value.length;
+    //input query length
+    for (var i = 0; i<n; i++) {
+        if(((coffees[i].toLowerCase()).indexOf(value.toLowerCase()))>-1)
+        {
+            //comparing if input string is existing in tags[i] string
+
+            var node = document.createElement("option");
+            var val = document.createTextNode(coffees[i]);
+            node.appendChild(val);
+
+            document.getElementById("datalist").appendChild(node);
+            //creating and appending new elements in data list
+        }
+    }
+}
+
 
 // Coffee Name Section Below
 
